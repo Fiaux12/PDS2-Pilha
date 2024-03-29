3a57aef2757850a2156463eb18cbe2f083e7224b
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-03-29 10:48:59 -0300

    create files lib

--- Pilha/Pilha.c
+++ Pilha/Pilha.c
@@ -77,25 +77,3 @@ void imprimePilha(Pilha *pilha)
         printf("%d\n", aux->valor);
     }
 }
-
-int main()
-{
-    printf("Inicializando pilha\n");
-    Pilha *novaPilha = criaPilha();
-    push(novaPilha, 1);
-    push(novaPilha, 2);
-    push(novaPilha, 3);
-    push(novaPilha, 4);
-    push(novaPilha, 5);
-
-    imprimePilha(novaPilha);
-    printf("--------------\n");
-    while (!pilhaVazia(novaPilha))
-    {
-        printf("popped: %d\n", pop(novaPilha));
-    }
-
-    printf("Liberando Pilha\n");
-    pilhaLibera(novaPilha);
-    return 0;
-}
